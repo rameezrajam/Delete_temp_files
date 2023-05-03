fefdbc9961cc5969c9d4914722ba8eafac7649cc
rameezrajam <109138183+rameezrajam@users.noreply.github.com>
2023-05-03 18:32:18 +0530

    Add files via upload

---
+++ delete_specific_filetype.ps1
@@ -0,0 +1,11 @@
+﻿# Set the folder path and file type you want to delete
+$FolderPath = "C:\Users\Administrator\Documents\ExampleFolder"
+$FileType = "*.txt"
+
+# Get all files in the folder with the specified file type
+$Files = Get-ChildItem -Path $FolderPath -Filter $FileType
+
+# Delete each file in the array
+foreach ($File in $Files) {
+    Remove-Item $File.FullName -Force
+}
