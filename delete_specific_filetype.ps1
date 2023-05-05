72587c67477d7d0c91f0fd878b2fa7d19a281215
rameezrajam <109138183+rameezrajam@users.noreply.github.com>
2023-05-05 13:03:28 +0530

    Update delete_specific_filetype.ps1

--- delete_specific_filetype.ps1
+++ delete_specific_filetype.ps1
@@ -1,7 +1,7 @@
 #$FileType = "*.txt"
 
 
-Write-Output "109-Backup Processing"
+Write-Output "user temp file clearing - Processing"
 Write-Output ""
 Write-Output "Please Don't close"
 
@@ -9,7 +9,7 @@ Write-Output "Please Don't close"
 # Get all files in the folder with the specified file type
 $Users = Get-ChildItem -Path "C:\Users\" -Name *
 foreach ($User in $Users){
- $Files = Get-ChildItem -Path "C:\Users\$user\AppData\Local\test" -ErrorAction SilentlyContinue # -Filter $FileType
+ $Files = Get-ChildItem -Path "C:\Users\$user\AppData\Local\temp" -ErrorAction SilentlyContinue # -Filter $FileType
 # Delete each file in the array
 foreach ($File in $Files) {
     Remove-Item $File.FullName -Force
